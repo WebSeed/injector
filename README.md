815d0f99e7faa8199e7b6c7fabb7f7b645817333
Will Bamford <w.bamford@gmail.com>
2016-07-15 16:12:10 +0100

    Basic working code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# injector
-Inject a responsive iframe into a page
+# Injector
+
+Inject a responsive iframe into a page.
+
+> We can use JavaScript to communicate between the iframe and the top-level page. With one slight problem — due to the same-origin policy an iframe at a different domain can't use JavaScript to directly communicate with the parent page.  Instead we have to use the postMessage() method to do cross-window messaging.
+
+https://www.advomatic.com/blog/responsive-iframes-yes-it-is-possible
+
+## References
+
+* https://www.advomatic.com/blog/responsive-iframes-yes-it-is-possible
--- inject.js
+++ inject.js
@@ -1,14 +1,31 @@
 (function(window, document) {
 
-  // window.addEventListener('load', function() {
-  // });
+  var mountSelector = '#mount';
+
+  var iframeSrc = window.location.protocol + '//' + window.location.host + '/target.html';
 
   var iframe = document.createElement('iframe');
   iframe.frameBorder = 0;
   iframe.width = '640px';
   iframe.height = '480px';
-  iframe.id = 'iframe-id';
-  iframe.setAttribute('src', 'http://www.bbc.co.uk/news');
-  document.getElementById('mount').appendChild(iframe);
+  iframe.id = 'the-id';
+  iframe.addEventListener('load', function(event) {
+    postMessage({ type: 'ready' });
+  });
+  iframe.setAttribute('src', iframeSrc);
+  document.querySelector(mountSelector).appendChild(iframe);
+
+  function onMessage(event) {
+    console.log('message event from target', event);
+    if (event.data.type === 'height') {
+      iframe.height = event.data.height + 'px';
+    }
+  }
+
+  function postMessage(data) {
+    iframe.contentWindow.postMessage(data, iframeSrc);
+  }
+
+  window.addEventListener('message', onMessage, false);
 
 }(window, document))
